e5b7636c65d3545ca4f5a5eec6467deffd4edf6b
jhy <idpc8090@gmail.com>
2025-12-24 11:14:35 +0900

    [refactor] vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'jh0-0y.github.io',
+  base: '/jh0-0y.github.io/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
